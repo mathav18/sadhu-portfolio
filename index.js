3180d2ec6300462f8936a0df12ea6506128ff97e
mathav18 <cmathavan34@gmail.com>
2023-03-23 17:47:12 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+function scratch() {
+    window.location.href = "https://scratch.mit.edu/projects/723415216";
+}
+function checkers() {
+    window.location.href = "https://javascriptfinalproject.sa-dhu17.repl.co/";
+}
+function chat() {
+    window.location.href = "https://github.com/SadhuSundarSingh/Websocket-project---Chat";
+}
+function bank() {
+    window.location.href = "https://github.com/SadhuSundarSingh/Console-project---Bank";
+}
+function bosscart() {
+    window.location.href = "https://github.com/SadhuSundarSingh/Boss-Cart";
+}
+function music() {
+    window.location.href = "https://music-player.sa-dhu17.repl.co/";
+}
+function dictionary() {
+    window.location.href = "https://github.com/SadhuSundarSingh/Dictionary";
+}
